e00550b7cd69deb0461e86532d4c28dad794804f
shreyyg <shreyag@andrew.cmu.edu>
2016-11-10 10:50:04 -0500

    resampling added
    resampling added

--- mp32mfcc.m
+++ mp32mfcc.m
@@ -1,7 +1,24 @@
-[speech,fs] = audioread('weeknd.mp3'); %insert song here
-speech = (speech(:,1)+speech(:,2))/2; %convert from stereo to mono
+filename = 'samp2.mp3';
+
+[speech,fs] = audioread(filename); %insert song here
+if fs ~= 44100
+    filename = strrep(filename,'.mp3','.wav');
+    audiowrite(filename,speech,44100);
+    [speech,fs] = audioread(filename);
+end
+
+speech = (speech(:,1)+speech(:,2))/2; % convert from stereo to mono
+
+
+
+
 [length dontcare] = size(speech);
-speech = speech(length/2 - 661500:length/2 + 661500);%grab audio from mmiddle 30 secs
+
+%fm ~= 1400kHz; fs = 44.1kHz -> fs << 2*fm
+
+[length dontcare] = size(speech);
+desiredSamp = 30/(1/fs);
+speech = speech(round(length/2 - round(desiredSamp/2)) : round(length/2 + (desiredSamp/2)));%grab audio from mmiddle 30 secs
 length = 30000; %length of sample in ms
 Tw = length/510;
 Ts = Tw*.5;
